e3c4ac43f4feef3a9591b7f6cbc71c36e781b91e
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-03-31 17:56:47 +0200

    Add recipe list item

---
+++ www/js/MyRecipesPage.js
@@ -0,0 +1,17 @@
+import Recipe from "./Recipe.js";
+
+const recipes = [new Recipe("Bananabread", ["breakfast", "lunch"], "180", "10", "img/Freshly_baked_banana_bread.jpg", ["250 g wheat flour", "2 tsp baking powder", "1 tsp ground cinnamon", "1 pinch of salt", "300 g of bananas", "100 g unsalted butter", "200 g caster sugar", "8 g vanilla sugar", "2 medium eggs", "125 ml crème fraîche"], "Preheat the oven to 180 °C. Grease the cake tin and line it with baking paper. In a large bowl, sift the flour, baking powder, cinnamon and salt. Peel and mash the banana. Mix the butter, caster and vanilla sugar in the food processor in 20 sec. until an almost smooth mixture. Turn and add the eggs and crème fraîche 1 by 1. Spoon through the flour mixture. Spoon the banana puree into the batter. Divide over the cake tin and bake for about 1 hour until golden brown and cooked. After 50 min. Check for doneness with the skewer. If it comes out clean, the cake is done. Allow to cool for 30 minutes, then remove from the mold and allow to cool further on a grid for 1 hour. Bon appétit!")];
+
+function UpdateRecipeList() {
+	// Get and clear the recipe container
+	var recipeContainer = document.getElementById("recipeContainer");
+	recipeContainer.innerHTML = '';
+	
+	for (var recipe of recipes) {
+		recipeContainer.appendChild(recipe.createListItem());
+	}
+}
+
+$(document).ready(function() {
+	UpdateRecipeList();
+});
--- www/js/Recipe.js
+++ www/js/Recipe.js
@@ -1,9 +1,71 @@
 export default class Recipe {
-	constructor(name, type, photo, ingredients, method) {
+	constructor(name, types, time, servings, image, ingredients, method) {
 		this.name = name;
-		this.type = type;
-		this.photo = photo;
+		this.types = types;
+		this.time = time;
+		this.servings = servings;
+		this.image = image;
 		this.ingredients = ingredients;
 		this.method = method;
+	}
+	
+	createListItem() {
+		// Create item element
+		var item = document.createElement("div");
+		item.className = "ListItem";
+		
+		// Create image container
+		var imageContainer = document.createElement("div");
+		imageContainer.className = "ImageContainer";
+		var image = document.createElement("img");
+		image.className = "image";
+		image.src = this.image;
+		imageContainer.appendChild(image);
+		item.appendChild(imageContainer);
+		
+		// Create name container
+		var nameContainer = document.createElement("p");
+		nameContainer.className = "NameContainer";
+		nameContainer.innerHTML = this.name;
+		item.appendChild(nameContainer);
+		
+		// Create type container
+		var typeContainer = document.createElement("p");
+		typeContainer.className = "TypeContainer";
+		for (var type of this.types) {
+			typeContainer.innerHTML += type + ", "
+		}
+		var typeString = typeContainer.innerHTML;
+		typeContainer.innerHTML = typeString.slice(0, -2);
+		item.appendChild(typeContainer)
+		
+		// Create time icon
+		var timeIcon = document.createElement("i");
+		timeIcon.className = "fas";
+		timeIcon.innerHTML = "&#xf017;";
+		item.appendChild(timeIcon);
+		
+		// Create time container
+		var timeContainer = document.createElement("p");
+		timeContainer.className = "TimeContainer";
+		timeContainer.innerHTML = this.time + " minutes";
+		item.appendChild(timeContainer);
+		
+		// Create break
+		item.appendChild(document.createElement("br"));
+		
+		// Create servings icon
+		var servingsIcon = document.createElement("i");
+		servingsIcon.className = "fas";
+		servingsIcon.innerHTML = "&#xf0c0;";
+		item.appendChild(servingsIcon);
+		
+		// Create servings container
+		var servingsContainer = document.createElement("p");
+		servingsContainer.className = "ServingsContainer";
+		servingsContainer.innerHTML = this.servings + " servings";
+		item.appendChild(servingsContainer);
+		
+		return item;
 	}
 }
