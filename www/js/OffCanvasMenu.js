6ad450e50f706ab43ae7bdd5b3f2ef88e10e0185
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-03-31 16:23:57 +0200

    Create NavBar

---
+++ www/js/OffCanvasMenu.js
@@ -0,0 +1,13 @@
+function openNav() {
+	var sideNavStyle = document.getElementById("sidenav").style;
+	sideNavStyle.width = "250px";
+	sideNavStyle.borderRight = "2px solid #FFB1A4";
+	document.getElementById("appContainer").style.opacity = 0.5;
+}
+
+function closeNav() {
+	var sideNavStyle = document.getElementById("sidenav").style;
+	sideNavStyle.width = "0";
+	sideNavStyle.border = "none";
+	document.getElementById("appContainer").style.opacity = 1;
+}
---
+++ www/js/Recipe.js
@@ -0,0 +1,9 @@
+export default class Recipe {
+	constructor(name, type, photo, ingredients, method) {
+		this.name = name;
+		this.type = type;
+		this.photo = photo;
+		this.ingredients = ingredients;
+		this.method = method;
+	}
+}
