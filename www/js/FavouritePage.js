c74f4cc0c6b69cd078f18848f79cb9ceb1d9e3f7
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-04-03 17:41:27 +0200

    Create favourite page

---
+++ www/js/FavouritePage.js
@@ -0,0 +1,49 @@
+import Recipe from "./Recipe.js";
+
+function UpdateRecipeList() {
+	// Get and clear the recipe container
+	var recipeContainer = document.getElementById("recipeContainer");
+	recipeContainer.innerHTML = '';
+	
+	// Retrieve the recipe list from localStorage
+	if (localStorage.favoriteRecipes != undefined) {
+		var list = JSON.parse(localStorage.favoriteRecipes);
+		
+		for (var serializedRecipe of list) {
+			DisplayRecipe(Recipe.unserialize(serializedRecipe));
+		}
+		
+	}
+	
+	$(".material-icons").on("click", function() {
+		this.parentElement.style.display = "none";
+	})
+}
+
+var filter = "All";
+
+function DisplayRecipe(recipe) {
+	if (filter != "All") {
+		if (!recipe.types.includes(filter)) return;
+	}
+	document.getElementById("recipeContainer").appendChild(recipe.createListItemWithHeartButton());
+}
+
+function filterButtonPress(buttonElement){
+	// Turn the text and border pink, for all filter buttons
+	$(".filterButton").css("color", "#FFB1A4");
+	$(".filterButton").css("border-bottom","none");
+	
+	// Turn text and border white, on the button I clicked 
+	buttonElement.style.color = "#F2F2F2";
+	buttonElement.style.borderBottom = "2px solid #F2F2F2";
+	
+	// Update the list
+	filter = buttonElement.innerHTML;
+	UpdateRecipeList();
+}
+
+$(document).ready(function() {
+	$(".filterButton").on("click", function() {filterButtonPress(this)});
+	UpdateRecipeList();
+});
