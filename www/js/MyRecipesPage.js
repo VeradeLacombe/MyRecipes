a98b36c71dddd6d2f33ec768f03fda5117c66a92
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-04-03 11:32:01 +0200

    Add filters

--- www/js/MyRecipesPage.js
+++ www/js/MyRecipesPage.js
@@ -22,10 +22,30 @@ function UpdateRecipeList() {
 	}
 }
 
+var filter = "All";
+
 function DisplayRecipe(recipe) {
+	if (filter != "All") {
+		if (!recipe.types.includes(filter)) return;
+	}
 	document.getElementById("recipeContainer").appendChild(recipe.createListItemWithHeartButton());
 }
 
+function filterButtonPress(buttonElement){
+	// Turn the text and border pink, for all filter buttons
+	$(".filterButton").css("color", "#FFB1A4");
+	$(".filterButton").css("border-bottom","none");
+	
+	// Turn text and border white, on the button I clicked 
+	buttonElement.style.color = "#F2F2F2";
+	buttonElement.style.borderBottom = "2px solid #F2F2F2";
+	
+	// Update the list
+	filter = buttonElement.innerHTML;
+	UpdateRecipeList();
+}
+
 $(document).ready(function() {
+	$(".filterButton").on("click", function() {filterButtonPress(this)});
 	UpdateRecipeList();
 });
