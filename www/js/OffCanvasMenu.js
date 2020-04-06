8337794400cd7bd540e6f87bfcf2a2fa028c49f0
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-04-06 19:27:05 +0200

    Fixed bug navbar

--- www/js/OffCanvasMenu.js
+++ www/js/OffCanvasMenu.js
@@ -4,14 +4,12 @@ $(document).ready(function() {
 
 function openNav() {
 	var sideNavStyle = document.getElementById("sidenav").style;
-	sideNavStyle.width = "250px";
-	sideNavStyle.borderRight = "2px solid #FFB1A4";
+	sideNavStyle.transform = "translateX(0)"
 	document.getElementById("appContainer").style.opacity = 0.5;
 }
 
 function closeNav() {
 	var sideNavStyle = document.getElementById("sidenav").style;
-	sideNavStyle.width = "0";
-	sideNavStyle.border = "none";
+	sideNavStyle.transform = "translateX(-250px)"
 	document.getElementById("appContainer").style.opacity = 1;
 }
