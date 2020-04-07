a3949514f2cc74f69b5d5b59aff88073d2307584
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-04-07 10:42:57 +0200

    Test loginPage

--- www/js/LoginPage.js
+++ www/js/LoginPage.js
@@ -1,9 +1,22 @@
-$(document).ready(function() {
+document.addEventListener("deviceready", onDeviceReadyEvent, false);
+
+function onDeviceReadyEvent() {
+	alert("onDeviceReady fires");
+	onDeviceReady();
+}
+
+function onDeviceReady() {
 	$("#form").submit(function(evt) {
+		alert("form submit fires");
 		evt.preventDefault();
 		if (document.getElementById("username").value == "") return;
 		if (document.getElementById("password").value == "") return;
 		localStorage.username = document.getElementById("username").value;
 		window.location.href = "MyRecipesPage.html";
 	})
+}
+
+$(document).ready(function() {
+	alert("$(document).ready fires")
+	onDeviceReady();
 })
