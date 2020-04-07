e22ef35704fd420e27d780b8bb5212a1cd1349b6
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-04-07 13:53:27 +0200

    Remove alerts from LoginPage.js

--- www/js/LoginPage.js
+++ www/js/LoginPage.js
@@ -1,25 +1,9 @@
-alert("Javascript runs");
-
-document.addEventListener("deviceready", onDeviceReadyEvent, false);
-
-function onDeviceReadyEvent() {
-	alert("onDeviceReadyEvent fires");
-	onDeviceReady();
-}
-
-function onDeviceReady() {
-	alert("onDeviceReady fires");
+$(document).ready(function() {
 	$("form").submit(function(evt) {
-		alert("form submit fires");
 		evt.preventDefault();
 		if (document.getElementById("username").value == "") return;
 		if (document.getElementById("password").value == "") return;
 		localStorage.username = document.getElementById("username").value;
 		window.location.href = "MyRecipesPage.html";
 	})
-}
-
-$(document).ready(function() {
-	alert("$(document).ready fires")
-	onDeviceReady();
 })
